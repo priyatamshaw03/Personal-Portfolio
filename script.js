cbe2bc54197242c645cae21425ad8c7484cd1a2a
Priyatam Shaw <priyatamshaw@gmail.com>
2025-08-17 13:37:08 +0530

    added projects

--- script.js
+++ script.js
@@ -46,22 +46,22 @@ navbar.classList.remove('active');
 
 
 /*========== swiper ==========*/
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 50,
-    loop: true,
-    grabCursor: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
+// var swiper = new Swiper(".mySwiper", {
+//     slidesPerView: 1,
+//     spaceBetween: 50,
+//     loop: true,
+//     grabCursor: true,
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
 
     
-});
+// });
 
 
 /*========== dark light mode ==========*/
